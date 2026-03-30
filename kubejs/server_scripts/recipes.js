4a97a80860aa55579559f1c9148c49567780a64d
redangel <redangel121@gmail.comgit config --global user.email redangel121@gmail.com>
2026-03-29 20:14:57 -0400

    finished recipe stripping

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,108 +1,9 @@
 // priority 2
 ServerEvents.recipes(event => {
     // NOTES:
-    // Register Chemicals: naquadria, silicon, blaze_gas, doped_silicon, boron_trifluoride, boron_trioxide, phosphorus, photoresist, tree_sap
     // Update the texture for Iron Spool to mimic the Copper Spool to use in the Vac Tube recipe
     // Add the other bars of chocolate from confectionary to the `c:foods/chocolate` tag
     // Add c:salt tag to other salt items for pams compat (tag + tag)
-
-    event.custom({
-        type: "mekanism:crystallizing",
-        input: {
-            amount: 1000,
-            chemical: "mekanism:tree_sap"
-        },
-        output: {
-            item: "pamhc2trees:maplesyrupitem"
-        }
-    });
-
-    // Remove easy rocket engines
-    event.remove("ad_astra:steel_engine");
-    event.remove("ad_astra:desh_engine");
-    event.remove("ad_astra:ostrum_engine");
-    event.remove("ad_astra:calorite_engine");
-    // Add hard engines
-    event.shaped(Item.of("ad_astra:steel_engine", 1), ["SSS", "ECE", " F "], {
-        S: "#c:plates/steel",
-        C: Item.RCU.getIdentifier(),
-        E: "ad_astra:engine_frame",
-        F: "ad_astra:engine_fan"
-    });
-    event.shaped(Item.of("ad_astra:desh_engine", 1), ["SSS", "ECE", " F "], {
-        S: "#c:plates/desh",
-        C: Item.RCU.getIdentifier(),
-        E: "ad_astra:engine_frame",
-        F: "ad_astra:engine_fan"
-    });
-    event.shaped(Item.of("ad_astra:ostrum_engine", 1), ["SSS", "ECE", " F "], {
-        S: "#c:plates/ostrum",
-        C: Item.RCU.getIdentifier(),
-        E: "ad_astra:engine_frame",
-        F: "ad_astra:engine_fan"
-    });
-    event.shaped(Item.of("ad_astra:calorite_engine", 1), ["SSS", "ECE", " F "], {
-        S: "#c:plates/calorite",
-        C: Item.RCU.getIdentifier(),
-        E: "ad_astra:engine_frame",
-        F: "ad_astra:engine_fan"
-    });
-
-    // Silicon Wafers
-    registerChemicalDissolutionRecipe(event, {
-        "amount": 10000,
-        "chemical": "mekanism:silicon"
-    }, {
-        amount: 10,
-        "chemical": "mekanism:blaze_gas"
-    }, {
-        "amount": 10,
-        ingredient: {
-            "item": "ae2:silicon"
-        }
-    });
-    registerChemicalInfusionRecipe(event, {
-        "amount": 10000,
-        "chemical": "mekanism:doped_silicon"
-    }, {
-        amount: 1,
-        "chemical": "mekanism:boron_trifluoride"
-    }, {
-        amount: 100,
-        "chemical": "mekanism:silicon"
-    });
-    registerCrystallizing(event, Item.SILICON_BOULE.getIdentifier(), {
-        "amount": 5000,
-        "chemical": "mekanism:doped_silicon"
-    });
-    registerItemTochemical(event, {
-        "amount": 100,
-        "chemical": "mekanism:phosphorus"
-    }, "c:phosphorus");
-    event.custom({
-        type: "mekanism:oxidizing",
-        input: {
-            ingredient: {
-                item: Item.PHOSPHORUS.getIdentifier()
-            }
-        },
-        output: {
-            amount: 200,
-            chemical: "mekanism:phosphorus"
-        }
-    });
-    registerItemTochemical(event, {
-        "amount": 250,
-        "chemical": "mekanism:blaze_gas"
-    }, "c:dusts/blaze");
-    registerItemTochemical(event, {
-        "amount": 1000,
-        "chemical": "mekanism:blaze_gas"
-    }, "c:rods/blaze");
-    registerItemTochemical(event, {
-        "amount": 5000,
-        "chemical": "mekanism:blaze_gas"
-    }, "c:eggs/blaze");
 
     // --- START MULTISTEP BULLSHIT ---
     // MOSFET
@@ -122,36 +23,6 @@ ServerEvents.recipes(event => {
         "amount": 1,
         "chemical": "mekanism:phosphorus"
     }, MultistepProcess.INTERMEDIATE_ITEM, Item.MOSFET_WAFER.getIdentifier())).usingItem(Item.INCOMPLETE_MOSFET_WAFER.getIdentifier()).register(event);
-    // Slicing
-    event.custom({
-        "type": "mekanism:sawing",
-        "input": {
-            "ingredient": {
-                "item": Item.MOSFET_WAFER.getIdentifier()
-            }
-        },
-        "mainOutput": {
-            "count": 32,
-            "item": Item.MOSFET_CHIP.getIdentifier()
-        }
-    });
-    event.custom({
-        "type": "mekanism:combining",
-        "extraInput": {
-            "ingredient": {
-                "item": "minecraft:copper_ingot"
-            }
-        },
-        "mainInput": {
-            "ingredient": {
-                "item": Item.MOSFET_CHIP.getIdentifier()
-            }
-        },
-        "output": {
-            "count": 1,
-            "item": Item.MOSFET.getIdentifier()
-        }
-    });
     // Integrated Circuit
     new MultistepProcess()
         // Oxide layer
@@ -221,71 +92,6 @@ ServerEvents.recipes(event => {
             "amount": 2,
             "chemical": "mekanism:phosphorus"
         }, MultistepProcess.INTERMEDIATE_ITEM, Item.INTEGRATED_CIRCUIT_WAFER.getIdentifier())).usingItem(Item.INCOMPLETE_IC.getIdentifier()).register(event);
-
-    // Boron stuff
-    event.custom({
-        type: "mekanism:dissolution",
-        chemicalInput: {
-            amount: 5,
-            chemical: "mekanism:sulfuric_acid"
-        },
-        itemInput: {
-            ingredient: {
-                tag: "c:ores/borax"
-            }
-        },
-        output: {
-            amount: 5000,
-            chemical: "mekanism:boron_trioxide"
-        }
-    });
-    event.custom({
-        type: "mekanism:chemical_infusing",
-        leftInput: {
-            amount: 6,
-            chemical: "mekanism:hydrofluoric_acid"
-        },
-        output: {
-            amount: 2,
-            chemical: "mekanism:boron_trifluoride"
-        },
-        rightInput: {
-            amount: 1,
-            chemical: "mekanism:boron_trioxide"
-        }
-    });
-    // photoresist
-    event.custom({
-        "type": "mekanism:oxidizing",
-        "input": {
-            "ingredient": {
-                "tag": "minecraft:saplings"
-            }
-        },
-        "output": {
-            "amount": 1000,
-            "chemical": "mekanism:tree_sap"
-        }
-    });
-    event.custom({
-        "type": "mekanism:oxidizing",
-        "input": {
-            "ingredient": {
-                "item": "pamhc2trees:maplesyrupitem"
-            }
-        },
-        "output": {
-            "amount": 1000,
-            "chemical": "mekanism:tree_sap"
-        }
-    });
-    registerChemicalDissolutionRecipe(event, {
-        "amount": 1000,
-        "chemical": "mekanism:photoresist"
-    }, {
-        "amount": 5,
-        "chemical": "mekanism:tree_sap"
-    }, "minecraft:glowstone_dust");
 
     //====Edible Processor====\\
     // Wafer recipe
@@ -417,28 +223,4 @@ ServerEvents.recipes(event => {
             "amount": 1,
             "chemical": "mekanism:phosphorus"
         }, MultistepProcess.INTERMEDIATE_ITEM, Item.ISOTOPIC_DECAY_OSCILLATOR_WAFER.getIdentifier())).usingItem(Item.INCOMPLETE_ISOTOPIC_DECAY_OSCILLATOR_WAFER.getIdentifier()).register(event);
-    event.custom({
-        "type": "mekanism:reaction",
-        "duration": 100,
-        "fluidInput": {
-            "amount": 100,
-            "fluid": "sgjourney:liquid_naquadah"
-        },
-        "chemicalInput": {
-            "amount": 25,
-            "chemical": "mekanism:plutonium"
-        },
-        "chemicalOutput": {
-            "amount": 1,
-            "chemical": "mekanism:naquadria"
-        },
-        "itemInput": {
-            "ingredient": {
-                item: "sgjourney:pure_naquadah"
-            }
-        },
-        "itemOutput": {
-            "item": "sgjourney:raw_naquadah"
-        }
-    });
 });
