cc220ae19ffa768e36e18b63cf049c0e7089ee11
redangel <redangel121@gmail.comgit config --global user.email redangel121@gmail.com>
2026-06-14 16:17:23 -0400

    final touches

--- kubejs/startup_scripts/infinite_cells.js
+++ kubejs/startup_scripts/infinite_cells.js
@@ -1,3 +1,5 @@
+// Essence Item Cells
+
 let essences = [
     "air", "water", "fire", "earth", "stone", "dirt",
     "wood", "ice", "deepslate", "nature", "dye", "nether",
@@ -17,67 +19,84 @@ let essences = [
 StartupEvents.registry("item", evnt => {
     for (let ID of essences) {
         evnt.create("sgcommunity_pack:" + ID + "_essence_cell", "custom_infinity_cell")
-        .itemType("mysticalagriculture:" + ID + "_essence")
-        .texture("sgcommunity_pack:item/inf_cells/" + ID + "_essence_cell")
-        .cellModel("sgcommunity_pack:block/drive/" + ID + "_essence_cell");
+            .itemType("mysticalagriculture:" + ID + "_essence")
+            .texture("sgcommunity_pack:item/inf_cells/" + ID + "_essence_cell")
+            .cellModel("sgcommunity_pack:block/drive/" + ID + "_essence_cell");
     }
 })
 
 // ====================================================================================================
+// Item Cells
+// NONE
+// ====================================================================================================
+// Fluid Cells
 // Add XP cell too
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:lava_cell", "custom_infinity_cell")
-            .fluidType("minecraft:lava")
-            .texture("sgcommunity_pack:item/inf_cells/lava_cell")
-            .cellModel("sgcommunity_pack:block/drive/lava_cell");
+    evnt.create("sgcommunity_pack:lava_cell", "custom_infinity_cell")
+        .fluidType("minecraft:lava")
+        .texture("sgcommunity_pack:item/inf_cells/lava_cell")
+        .cellModel("sgcommunity_pack:block/drive/lava_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:milk_cell", "custom_infinity_cell")
-            .fluidType("minecraft:milk")
-            .texture("sgcommunity_pack:item/inf_cells/milk_cell")
-            .cellModel("sgcommunity_pack:block/drive/milk_cell");
+    evnt.create("sgcommunity_pack:milk_cell", "custom_infinity_cell")
+        .fluidType("minecraft:milk")
+        .texture("sgcommunity_pack:item/inf_cells/milk_cell")
+        .cellModel("sgcommunity_pack:block/drive/milk_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:honey_cell", "custom_infinity_cell")
-            .fluidType("create:honey")
-            .texture("sgcommunity_pack:item/inf_cells/honey_cell")
-            .cellModel("sgcommunity_pack:block/drive/honey_cell");
+    evnt.create("sgcommunity_pack:honey_cell", "custom_infinity_cell")
+        .fluidType("create:honey")
+        .texture("sgcommunity_pack:item/inf_cells/honey_cell")
+        .cellModel("sgcommunity_pack:block/drive/honey_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:chocolate_cell", "custom_infinity_cell")
-            .fluidType("create:chocolate")
-            .texture("sgcommunity_pack:item/inf_cells/chocolate_cell")
-            .cellModel("sgcommunity_pack:block/drive/chocolate_cell");
+    evnt.create("sgcommunity_pack:chocolate_cell", "custom_infinity_cell")
+        .fluidType("create:chocolate")
+        .texture("sgcommunity_pack:item/inf_cells/chocolate_cell")
+        .cellModel("sgcommunity_pack:block/drive/chocolate_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:ruby_chocolate_cell", "custom_infinity_cell")
-            .fluidType("create_confectionery:ruby_chocolate")
-            .texture("sgcommunity_pack:item/inf_cells/ruby_chocolate_cell")
-            .cellModel("sgcommunity_pack:block/drive/ruby_chocolate_cell");
+    evnt.create("sgcommunity_pack:ruby_chocolate_cell", "custom_infinity_cell")
+        .fluidType("create_confectionery:ruby_chocolate")
+        .texture("sgcommunity_pack:item/inf_cells/ruby_chocolate_cell")
+        .cellModel("sgcommunity_pack:block/drive/ruby_chocolate_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:black_chocolate_cell", "custom_infinity_cell")
-            .fluidType("create_confectionery:black_chocolate")
-            .texture("sgcommunity_pack:item/inf_cells/black_chocolate_cell")
-            .cellModel("sgcommunity_pack:block/drive/black_chocolate_cell");
+    evnt.create("sgcommunity_pack:black_chocolate_cell", "custom_infinity_cell")
+        .fluidType("create_confectionery:black_chocolate")
+        .texture("sgcommunity_pack:item/inf_cells/black_chocolate_cell")
+        .cellModel("sgcommunity_pack:block/drive/black_chocolate_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:white_chocolate_cell", "custom_infinity_cell")
-            .fluidType("create_confectionery:white_chocolate")
-            .texture("sgcommunity_pack:item/inf_cells/white_chocolate_cell")
-            .cellModel("sgcommunity_pack:block/drive/white_chocolate_cell");
+    evnt.create("sgcommunity_pack:white_chocolate_cell", "custom_infinity_cell")
+        .fluidType("create_confectionery:white_chocolate")
+        .texture("sgcommunity_pack:item/inf_cells/white_chocolate_cell")
+        .cellModel("sgcommunity_pack:block/drive/white_chocolate_cell");
 })
 
 StartupEvents.registry("item", evnt => {
-        evnt.create("sgcommunity_pack:dt_fuel_cell", "custom_infinity_cell")
-            .fluidType("mekanismgenerators:fusion_fuel")
-            .texture("sgcommunity_pack:item/inf_cells/dt_fuel_cell")
-            .cellModel("sgcommunity_pack:block/drive/dt_fuel_cell");
+    evnt.create("sgcommunity_pack:dt_fuel_cell", "custom_infinity_cell")
+        .fluidType("mekanismgenerators:fusion_fuel")
+        .texture("sgcommunity_pack:item/inf_cells/dt_fuel_cell")
+        .cellModel("sgcommunity_pack:block/drive/dt_fuel_cell");
 })
+
+// ====================================================================================================
+// Chem Cells, CRASHING WITH ERROR: Java class "mekanism.common.registries.MekanismChemicals" has no public instance field or method named "FUSION_FUEL".
+
+// const MekKey$ = Java.loadClass("me.ramidzkh.mekae2.ae2.MekanismKey")
+// const Chem$ = Java.loadClass("mekanism.common.registries.MekanismChemicals")
+// 
+// StartupEvents.registry('item', evnt => {
+//     evnt.create("sgcommunity_pack:dt_fuel_cell", "custom_infinity_cell")
+//         .type(() => MekKey$.of(Chem$.FUSION_FUEL.getStack(1000)))
+//         .texture("sgcommunity_pack:item/inf_cells/dt_fuel_cell")
+//         .cellModel("sgcommunity_pack:block/drive/dt_fuel_cell");
+// })
